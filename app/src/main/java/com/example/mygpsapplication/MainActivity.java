a16d170cf9b3c40c52fe6a9f6e41969f34b06b58
ADITYA <varalemcoe@gmail.com>
2021-04-12 00:01:08 +0530

    Initial Release(Fused Location)

--- app/src/main/java/com/example/mygpsapplication/MainActivity.java
+++ app/src/main/java/com/example/mygpsapplication/MainActivity.java
@@ -1,14 +1,77 @@
 package com.example.mygpsapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_location;
+    private TextView tv_lat, tv_long;
+    private FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        }
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+        findViewByIds();
+
+        justCallButton();
+    }
+
+    private void findViewByIds() {
+        btn_location = findViewById(R.id.btn_location);
+        tv_lat = findViewById(R.id.tv_lat);
+        tv_long = findViewById(R.id.tv_long);
+    }
+
+    private void justCallButton() {
+        btn_location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+                    return;
+                }
+
+                fusedLocationProviderClient.getLastLocation().addOnSuccessListener(MainActivity.this,
+                        new OnSuccessListener<Location>() {
+                            @Override
+                            public void onSuccess(Location location) {
+
+                                if(location != null) {
+                                    tv_lat.setText(String.valueOf(location.getLatitude()));
+                                    tv_long.setText(String.valueOf(location.getLongitude()));
+                                }
+                            }
+                        });
+                }
+        });
     }
 }
